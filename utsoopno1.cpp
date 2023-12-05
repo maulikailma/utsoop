1b37f2fb1edf145770b83070c1ddf5c9826cef3e
unknown <ilmamaulika@gmail.com>
2023-12-05 09:46:10 +0700

    uts oop no 1

---
+++ utsoopno1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class PersegiPanjang {
+public:
+    double panjang;
+    double lebar;
+
+public:
+    // Setter
+    void setPanjang(double p = 0.0 ) { panjang = p; }
+    void setLebar(double l = 0.0 ) { lebar = l; }
+
+    // Getter
+    double getPanjang() const { return panjang; }
+    double getLebar() const { return lebar; }
+
+    // Menghitung luas dan keliling
+    double hitungLuas() const { return panjang * lebar; }
+    double hitungKeliling() const { return 2 * (panjang + lebar); }
+};
+
+int main() {
+    PersegiPanjang persegi;
+    
+    // Mengatur panjang dan lebar
+    persegi.setPanjang(16);
+    persegi.setLebar(18);
+
+    // Menampilkan panjang, lebar, luas, dan keliling
+    std::cout << "Panjang: " << persegi.getPanjang() << " Lebar: " << persegi.getLebar() << std::endl;
+    std::cout << "Luas: " << persegi.hitungLuas() << std::endl;
+    std::cout << "Keliling: " << persegi.hitungKeliling() << std::endl;
+
+    return 0;
+}
